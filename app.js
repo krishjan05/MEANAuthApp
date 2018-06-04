3fdf4a2aff03e26975ef9cedd7d5d5f343e34d7c
Krishna Kanhaiya <krishjan05@gmail.com>
2018-06-03 20:35:26 -0400

    Completed basic structure for MEAN application.

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Getting the configuration for database
+mongoose.connect(config.database);
+
+//Mongo DB successfull connection
+mongoose.connection.on('connected', () => {
+	console.log('Connected to databse ' + config.database);
+});
+
+//Mongo DB connection Error
+mongoose.connection.on('error', (err) => {
+	console.log('Database Error ' + err);
+});
 
 const app = express();
 const users = require('./routes/users');
@@ -12,6 +26,9 @@ const port = 3000;
 
 
 app.use(cors());
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Body Parser Middleware
 app.use(bodyParser.json());
---
+++ config/database.js
@@ -0,0 +1,5 @@
+//Configuration of datbase
+module.exports = {
+	database: 'mongodb://localhost:27017/meanauth',
+	secret : 'mysecret',
+};
