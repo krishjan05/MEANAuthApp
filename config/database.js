f99bf9d190d8f0f48d84f08901603939cd9902f9
Krishna Kanhaiya <krishjan05@gmail.com>
2018-06-23 22:16:37 -0400

    changed db connection string

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 //Configuration of datbase
 module.exports = {
-	database: 'mongodb://localhost:27017/meanauth',
+	// database: 'mongodb://localhost:27017/meanauth',
+	database: 'ds215961.mlab.com:15961/meanauthapp -u krishjan05 -p Krishna@123',
 	secret : 'mysecret',
 };
