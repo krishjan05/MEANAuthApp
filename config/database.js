640a819171da72fd5ffda7909c29369ffcd0cb34
Krishna Kanhaiya <krishjan05@gmail.com>
2018-06-24 00:32:14 -0400

    updated connection string

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //Configuration of datbase
 module.exports = {
 	// database: 'mongodb://localhost:27017/meanauth',
-	database: 'mongodb://krishjan05:Krishna@123@ds215961.mlab.com:15961/meanauthapp',
+	database: 'mongodb://admin:admin123@ds215961.mlab.com:15961/meanauthapp',
 	secret : 'mysecret',
 };
